0bf2a2ab3832b4192b4a2d92b4c124c7fc9d464f
Jiahao Cai <cai.jiah@husky.neu.edu>
2021-02-20 18:55:53 -0500

    finished heading

--- src/component/course-manager/course-manager.js
+++ src/component/course-manager/course-manager.js
@@ -1,4 +1,6 @@
 import React from "react";
+import api from "../../services/course-service";
+import "./course-manager.css"
 
 export default class CourseManager
     extends React.Component {
@@ -6,17 +8,17 @@ export default class CourseManager
         return (
             <div className="sticky-nav sticky-nav-padding">
                 <div className="row sticky-nav-row">
-                    <div className="col-md-1 col">
-                        <i id="expand-button" className="fa fa-bars fa-2x"></i>
+                    <div className="col-lg-1 col">
+                        <i className="fas fa-bars fa-2x"/>
                     </div>
-                    <div className="col-md-2 col-2 d-none d-md-block">
+                    <div className="col-lg-3 col-2 d-none d-lg-block">
                         <h5>Course Manager</h5>
                     </div>
-                    <div className="col-md-8 col-6 search">
+                    <div className="col-lg-7 col-md-8 col-6 search">
                         <input id="search-bar" className="form-control"
                                placeholder="New Course Title"/>
                     </div>
-                    <div className="col-md-1 col plus-button">
+                    <div className="col-lg-1 col plus-button">
                         <span className="fa-stack fa-lg">
                             <i className="fa fa-circle fa-stack-2x"></i>
                             <i className="fa fa fa-plus fa-stack-1x"></i>
