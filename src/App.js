5e3586a56f4fbcea52da6abd76fd509ec4bb2936
Jiahao Cai <cai.jiah@husky.neu.edu>
2021-02-20 18:57:01 -0500

    don't put heading into a container

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import logo from './logo.svg';
-import './App.css';
 import {BrowserRouter, Route} from "react-router-dom";
+import CourseManager from "./component/course-manager/course-manager";
 import Home from "./component/home"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import './App.css';
+
 function App() {
   return (
       <BrowserRouter>
-        <div className="container-fluid">
-          <Route path="/" exact={true} component={Home}></Route>
-        </div>
+          <Route path="/" exact={true} component={Home}/>
+          <Route path="/courses" component={CourseManager}/>
+          <div className="container-fluid">
+
+          </div>
       </BrowserRouter>
   );
 }
