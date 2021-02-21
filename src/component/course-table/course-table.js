c1879fcd0c30ddc354e37881e490a764dea821db
Jiahao Cai <cai.jiah@husky.neu.edu>
2021-02-20 23:55:44 -0500

    worked on building course table

---
+++ src/component/course-table/course-table.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import CourseRow from "../course-row/course-row";
+import {Link} from "react-router-dom";
+import "./course-table.css"
+
+export default class CourseTable extends
+    React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        console.log(this.props.courses)
+        return(
+            <div>
+                {/*<Link to="/courses/grid">*/}
+                {/*    <i className="fas fa-th float-right fa-2x"></i>*/}
+                {/*</Link>*/}
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th className="col-md-6">Title</th>
+                            <th className="col-md-2">Owned by</th>
+                            <th className="col-md-2">Last modified</th>
+                            <th className="col-md-2">
+                                <span className="float-right button-header">
+                                    <i className="fas fa-folder"
+                                       aria-hidden="true"/>
+                                    <i className="fas fa-sort-alpha-up-alt"
+                                       aria-hidden="true"/>
+                                    <i className="fas fa-th"
+                                       aria-hidden="true"/>
+                                </span>
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    {
+                        this.props.courses.map(course =>
+                                                   <CourseRow
+                                                       key={course._id}
+                                                       deleteCourse={this.props.deleteCourse}
+                                                       updateCourse={this.props.updateCourse}
+                                                       course={course}
+                                                       title={course.title}
+                                                       lastModified={course.lastModified}
+                                                       owner={course.owner}/>)
+                    }
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+}
