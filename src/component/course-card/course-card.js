31f0292686b7bdae9c7ae248aed9f1f8013a21d0
Jiahao Cai <cai.jiah@husky.neu.edu>
2021-02-21 16:46:30 -0500

    Implemented course card as function which has edit buttons.

---
+++ src/component/course-card/course-card.js
@@ -0,0 +1,68 @@
+import React, {useState} from 'react'
+import {Link} from "react-router-dom";
+import "./course-card.css"
+
+const CourseCard = (
+    {
+        course,
+        deleteCourse,
+        updateCourse}) => {
+    const [editing, setEditing] = useState(false)
+    const [title, setTitle] = useState(course.title)
+
+    const saveCourse = () => {
+        setEditing(false)
+        const newCourse = {
+            ...course,
+            title: title
+        }
+        updateCourse(newCourse)
+    }
+
+    return (
+        <div className="col mb-4">
+            <div className="card h-100">
+                {
+                    editing&&
+                    <span className="top-left-buttons">
+                        <i onClick={() => saveCourse()}
+                           className="fas fa-check confirm-button"/>
+                        <i onClick={() => deleteCourse(course)}
+                           className="fas fa-times delete-button"/>
+                    </span>
+                }
+                <img src="https://www.valuecoders.com/blog/wp-content/uploads/2016/08/react.png"
+                     className="card-img-top" alt="..."/>
+                <div className="card-body">
+                    {
+                        !editing &&
+                        <h5 className="card-title">{course.title}</h5>
+                    }
+
+                    {
+                        editing &&
+                        <input
+                            className="form-control"
+                            onChange={(e) => setTitle(e.target.value)}
+                            value={title}/>
+                    }
+                    <p className="card-text">
+                        Some description.</p>
+                    <Link to="/editor" className="btn btn-primary">
+                        {course.title}
+                    </Link>
+
+                    {
+                        !editing &&
+                        <i onClick={() => setEditing(true)}
+                           className="fas fa-edit edit-button"/>
+                    }
+
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default CourseCard
