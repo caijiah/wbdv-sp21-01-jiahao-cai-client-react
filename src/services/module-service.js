1d2a4297bff86bdad6fcf158e871097dbefee077
Jiahao Cai <cai.jiah@husky.neu.edu>
2021-03-09 03:39:59 -0500

    implement create module

--- src/services/module-service.js
+++ src/services/module-service.js
@@ -4,8 +4,19 @@ const findModulesForCourse = (courseId) =>
     fetch(`${COURSES_URL}/${courseId}/modules`)
         .then(response => response.json())
 
+const createModule = (courseId, module) =>
+    fetch(`${COURSES_URL}/${courseId}/modules`, {
+        method:"POST",
+        body: JSON.stringify(module),
+        headers : {
+            'content-type': 'application/json'
+        }
+    })
+        .then(response => response.json())
+
 const moduleApi = {
-    findModulesForCourse
+    findModulesForCourse,
+    createModule
 }
 
 export default moduleApi
