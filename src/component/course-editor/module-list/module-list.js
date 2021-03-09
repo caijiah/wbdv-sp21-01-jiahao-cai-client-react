ece643d031feeca529a530447feb89c288756176
Jiahao Cai <cai.jiah@husky.neu.edu>
2021-03-09 03:11:55 -0500

    make it route to correct module

--- src/component/course-editor/module-list/module-list.js
+++ src/component/course-editor/module-list/module-list.js
@@ -2,27 +2,33 @@ import React from 'react'
 import {connect} from "react-redux"
 import "./module-list.css"
 import EditableItem from "../../editable-item";
+import {useParams} from "react-router-dom";
+
 
 const ModuleList = ({modules=[],
                     createModule,
                     updateModule,
-                    deleteModule}) =>
-    <ul className="list-group">
-        {
-            modules.map(module =>
-            <li
-                key={module._id}
-                className="list-group-item">
-                <EditableItem
-                    item={module}
-                    updateItem={updateModule}
-                    deleteItem={deleteModule}/>
-            </li>)
-        }
-        <li className="list-group-item addCourse-button">
-            <i onClick={createModule} className="fa fa-plus fa-2x"/>
-        </li>
-    </ul>
+                    deleteModule}) => {
+    const {layout, courseId, moduleId} = useParams()
+    return (
+        <ul className="list-group">
+            {
+                modules.map(module =>
+                            <li
+                                key={module._id}
+                                className="list-group-item">
+                                <EditableItem
+                                    to={`/courses/${layout}/edit/${courseId}/modules/${module._id}`}
+                                    item={module}
+                                    updateItem={updateModule}
+                                    deleteItem={deleteModule}/>
+                            </li>)
+            }
+            <li className="list-group-item addCourse-button">
+                <i onClick={createModule} className="fa fa-plus fa-2x"/>
+            </li>
+        </ul>)
+}
 
 const stpm = (state) => {
     return {
