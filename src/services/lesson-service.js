3dcd8aca3165bfb9e3268c7d4830d128156d922a
Jiahao Cai <cai.jiah@husky.neu.edu>
2021-03-09 07:34:29 -0500

    init lesson service and implemented findLessons, createLesson, deleteLesson

---
+++ src/services/lesson-service.js
@@ -0,0 +1,29 @@
+const MODULES_URL = "https://wbdv-generic-server.herokuapp.com/api/001697284/modules";
+const LESSONS_URL = "https://wbdv-generic-server.herokuapp.com/api/001697284/lessons"
+
+const findLessonsForModule = (moduleId) =>
+    fetch(`${MODULES_URL}/${moduleId}/lessons`)
+        .then(response => response.json())
+
+const createLesson = (moduleId, lesson) =>
+    fetch(`${MODULES_URL}/${moduleId}/lessons`,{
+        method: "POST",
+        body: JSON.stringify(lesson),
+        headers: {
+            'content-type': 'application/json'
+        }
+    })
+        .then(response => response.json())
+
+const deleteLesson = (lessonId) =>
+    fetch(`${LESSONS_URL}/${lessonId}`,{
+        method: "DELETE"
+    })
+        .then(response => response.json())
+
+const api = {
+    findLessonsForModule,
+    createLesson
+}
+
+export default api
