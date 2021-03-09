1e34e9237d82ceaaad78bb2751557694eb7e7a7f
Jiahao Cai <cai.jiah@husky.neu.edu>
2021-03-09 07:36:11 -0500

    map dispatch to reducer

--- src/reducer/lesson-reducer.js
+++ src/reducer/lesson-reducer.js
@@ -1,14 +1,22 @@
 const initialState = {
-    lessons: [
-        {title: 'Lesson 1', _id: '123'},
-        {title: 'Lesson 2', _id: '234'},
-        {title: 'Lesson 3', _id: '345'}
-    ]
+    lessons: []
 }
 
 const lessonReducer = (state= initialState, action) => {
     switch (action.type) {
+        case "FIND_LESSONS_FOR_MODULE":
+            return {
+                ...state,
+                lessons: action.lessons
+            }
         case "CREATE_LESSON":
+            return {
+                ...state,
+                lessons: [
+                    ...state.lessons,
+                    action.lesson
+                    ]
+            }
         case "DELETE_LESSON":
         case "UPDATE_LESSON":
         default:
