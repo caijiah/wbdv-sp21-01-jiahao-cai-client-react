42e4513f523b46b97af3a45c51c2a7f39c87717f
Jiahao Cai <cai.jiah@husky.neu.edu>
2021-03-08 21:51:42 -0500

    create module list using dummy module data

---
+++ src/component/module-list/module-list.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import {connect} from "react-redux"
+
+const ModuleList = ({modules}) =>
+    <ul className="list-group">
+        {
+            modules.map(module =>
+            <li
+                key={module._id}
+                className="list-group-item">
+                {module.title}
+                <i className="float-right fa fa-trash"/>
+            </li>)
+        }
+        {/*<li className="list-group-item active">*/}
+        {/*    Module 1 - JQuery*/}
+        {/*    <i className="pull-right fa fa-trash"/>*/}
+        {/*</li>*/}
+
+    </ul>
+
+const stpm = (state) => {
+    return {
+        modules: state.modules
+    }
+}
+
+const dtpm = (dispath) => {}
+
+export default connect(stpm, dtpm)
+(ModuleList)
