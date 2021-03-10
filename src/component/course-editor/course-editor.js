0b87cb556111c152bacbe65c88310ca771d1e7b7
Jiahao Cai <cai.jiah@husky.neu.edu>
2021-03-09 21:42:16 -0500

    add topic reducer and use topicPills

--- src/component/course-editor/course-editor.js
+++ src/component/course-editor/course-editor.js
@@ -3,15 +3,18 @@ import {Link, useParams} from "react-router-dom";
 import "./course-editor.css"
 import moduleReducer from "../../reducer/module-reducer"
 import lessonReducer from "../../reducer/lesson-reducer";
+import topicReducer from "../../reducer/topic-reducer";
 import {combineReducers, createStore} from "redux";
 import {Provider} from "react-redux";
 import ModuleList from "./module-list/module-list";
 import LessonTabs from "./lesson-tabs/lesson-tabs";
+import topicPills from "./topic-pills/topic-pills"
 import {findCourseById} from "../../services/course-service";
 
 const reducer = combineReducers({
         moduleReducer: moduleReducer,
-        lessonReducer: lessonReducer
+        lessonReducer: lessonReducer,
+        topicReducer: topicReducer
     })
 
 const store = createStore(reducer)
@@ -44,29 +47,7 @@ const CourseEditor = ({history}) => {
                         <div className="col-9">
                             <LessonTabs/>
                             <br/>
-
-                            <ul className="nav nav-pills">
-                                <li className="nav-item">
-                                    <a className="nav-link active" aria-current="page" href="#">Topic 1</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#">Topic 2</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#">Topic 3</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link disabled" href="#" tabIndex="-1"
-                                       aria-disabled="true">
-                                        Topic 4
-                                    </a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#" tabIndex="-1" aria-disabled="true">
-                                        <i className="fa fa-plus"/>
-                                    </a>
-                                </li>
-                            </ul>
+                            <topicPills/>
                         </div>
                     </div>
                     <div className="mb-3 row">
