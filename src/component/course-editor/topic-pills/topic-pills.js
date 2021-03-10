aac989f042e9d5e3d72169a5832b881d30e86799
Jiahao Cai <cai.jiah@husky.neu.edu>
2021-03-09 21:43:09 -0500

    implement topic pills, note that if we go back to modules without selecting lesson, we should clear the topics

---
+++ src/component/course-editor/topic-pills/topic-pills.js
@@ -0,0 +1,86 @@
+import React, {useEffect} from 'react'
+import {useParams} from 'react-router-dom'
+import {connect} from 'react-redux'
+import EditableItem from "../../editable-item";
+import topicService from "../../../services/topic-service"
+
+const TopicPills = ({topics=[],
+                    findTopicsForLesson,
+                    createTopicForLesson,
+                    updateTopic,
+                    clearTopics,
+                    deleteTopic}) => {
+    const {layout, courseId, moduleId, lessonId, topicId} = useParams()
+    useEffect(()=> {
+        if (moduleId !== "undefined" && typeof moduleId !== "undefined"
+            &&
+            lessonId !== "undefined" && typeof lessonId !== "undefined") {
+            findTopicsForLesson(lessonId)
+        } else {
+            clearTopics()
+        }
+    },[moduleId, lessonId])
+    return (
+        <ul className='nav nav-pills'>
+            {
+                topics.map(topic =>
+                               <EditableItem
+                               key={topic._id}
+                               updateItem={updateTopic}
+                               deleteItem={deleteTopic}
+                               to={`/courses/${layout}/edit/${courseId}/modules/${moduleId}/lessons/${lessonId}/topics/${topicId}`}
+                               item={topic}/>
+                )
+            }
+            <li>
+                <i onClick={() => createTopicForLesson(lessonId)}
+                   className="fas fa-plus addTopic-button"/>
+            </li>
+        </ul>
+    )
+}
+
+const stpm = (state) => {
+    return {
+        topics: state.topicReducer.topics
+    }
+}
+
+const dtpm = (dispatch) => {
+    return {
+        findTopicsForLesson: (lessonId) => {
+            topicService.findTopicsForLesson(lessonId)
+                .then(topics => dispatch({
+                    type: "FIND_TOPICS_FOR_LESSON",
+                    topics
+                }))
+        },
+        createTopicForLesson: (lessonId) => {
+            topicService.createTopic(lessonId,
+                                     {title:"New Topic"})
+                .then(topic => dispatch({
+                    type: "CREATE_TOPIC",
+                    topic
+                }))
+        },
+        updateTopic: (newItem) => {
+            topicService.updateTopic(newItem._id, newItem)
+                .then(status => dispatch({
+                    type: "UPDATE_TOPIC",
+                    updatedTopic: newItem
+                }))
+        },
+        deleteTopic: (topicToDelete) => {
+            topicService.deleteTopic(topicToDelete._id)
+                .then(status => dispatch({
+                    type: "DELETE_TOPIC",
+                    topicToDelete
+                }))
+        },
+        clearTopics: () =>
+            dispatch({
+                type: "CLEAR_TOPICS"
+            })
+    }
+}
+export default connect(stpm, dtpm)(TopicPills)
