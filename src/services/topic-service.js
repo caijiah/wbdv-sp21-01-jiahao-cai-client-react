1382e29d7057f6db52839b1f9a97e84445092f81
Jiahao Cai <cai.jiah@husky.neu.edu>
2021-03-09 20:53:34 -0500

    implement topic service

---
+++ src/services/topic-service.js
@@ -0,0 +1,41 @@
+const LESSONS_URL = ' https://wbdv-generic-server.herokuapp.com/api/001697284/lessons'
+const TOPICS_URL = ' https://wbdv-generic-server.herokuapp.com/api/001697284/topics'
+
+const findTopicsForLesson = (lessonId) =>
+    fetch(`${LESSONS_URL}/${lessonId}/topics`)
+        .then(response => response.json())
+
+const createTopic = (lessonId, topic) =>
+    fetch(`${LESSONS_URL}/${lessonId}/topics`, {
+        method:"POST",
+        body: JSON.stringify(topic),
+        headers : {
+            'content-type': 'application/json'
+        }
+    })
+        .then(response => response.json())
+
+const deleteTopic = (topicId) =>
+    fetch(`${LESSONS_URL}/${topicId}`, {
+        method:"DELETE"
+    })
+        .then(response => response.json())
+
+const updateTopic = (topicId, topic) =>
+    fetch(`${TOPICS_URL}/${topicId}`, {
+        method:"PUT",
+        body: JSON.stringify(topic),
+        headers : {
+            'content-type': 'application/json'
+        }
+    })
+        .then(response => response.json())
+
+const moduleApi = {
+    findTopicsForLesson,
+    createTopic,
+    deleteTopic,
+    updateTopic
+}
+
+export default moduleApi
