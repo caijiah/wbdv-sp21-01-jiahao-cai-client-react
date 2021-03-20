b6eb3f312f0ca2cb18a687038f6f68cae2736f19
Jiahao Cai <cai.jiah@husky.neu.edu>
2021-03-20 19:44:35 -0400

    finish widget reducer

--- src/reducer/widget-reducer.js
+++ src/reducer/widget-reducer.js
@@ -5,9 +5,36 @@ const initialState = {
 const widgetReducer = (state = initialState, action) => {
     switch (action.type) {
         case "CREATE_WIDGET":
+            return {
+                ...state,
+                widgets: [
+                    ...state.widgets,
+                    action.widget
+                ]
+            }
         case "DELETE_WIDGET":
+            return {
+                ...state,
+                widgets: state.widgets.filter(widget => {
+                    return widget.id !== action.widgetToDelete.id
+                })
+            }
         case "UPDATE_WIDGET":
+            return {
+                ...state,
+                widgets: state.widgets.map(widget => {
+                    if (widget.id === action.updatedWidget.id) {
+                        return action.updatedWidget
+                    } else {
+                        return widget
+                    }
+                })
+            }
         case "FIND_WIDGETS_FOR_TOPIC":
+            return {
+                ...state,
+                widgets: action.widgets
+            }
         case "CLEAR_WIDGETS":
             return {
                 ...state,
