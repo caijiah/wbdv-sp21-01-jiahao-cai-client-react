19018b0d44ee8484169c16624daf6fc858e04c95
Jiahao Cai <cai.jiah@husky.neu.edu>
2021-03-21 02:44:23 -0400

    make it more clear

--- src/component/widgets/widget-type-dropdown/widget-type-dropdown.js
+++ src/component/widgets/widget-type-dropdown/widget-type-dropdown.js
@@ -5,7 +5,8 @@ const WidgetTypeDropdown = ({widget,
     return (
         <select value={widget.type}
                 className="col-10 form-control"
-                onChange={(e)=> getTypeChange(e.target.value)}>
+                onChange={(e)=>
+                    getTypeChange(e.target.value)}>
             <option value={"HEADING"}>Heading</option>
             <option value={"PARAGRAPH"}>Paragraph</option>
         </select>
