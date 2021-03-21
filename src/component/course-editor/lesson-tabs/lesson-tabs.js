bab1e992f85b34bb598cdf8dbeede6192874950f
Jiahao Cai <cai.jiah@husky.neu.edu>
2021-03-21 03:35:11 -0400

    make lesson tabs use lessonActions

--- src/component/course-editor/lesson-tabs/lesson-tabs.js
+++ src/component/course-editor/lesson-tabs/lesson-tabs.js
@@ -4,6 +4,7 @@ import {connect} from 'react-redux'
 import EditableItem from "../../editable-item";
 import lessonService from "../../../services/lesson-service"
 import "./lesson-tabs.css"
+import lessonActions from "../../../actions/lesson-actions";
 
 const LessonTabs = ({   lessons=[],
                         findLessonsForModule,
@@ -61,42 +62,12 @@ const stpm = (state) => {
 
 const dtpm = (dispatch) => {
     return {
-        findLessonsForModule: (moduleId) => {
-            lessonService.findLessonsForModule(moduleId)
-                .then(lessons => dispatch({
-                                             type: "FIND_LESSONS_FOR_MODULE",
-                                             lessons: lessons
-                                         }))
-        },
-        createLessonForModule: (moduleId) => {
-            lessonService.createLesson(moduleId, {title: "New Lesson"})
-                .then(lesson => dispatch({
-                                            type: "CREATE_LESSON",
-                                            lesson
-                                        }))
-        },
-        updateLesson: (newItem) => {
-            lessonService.updateLesson(newItem._id, newItem)
-                .then(status => dispatch(
-                    {
-                        type: "UPDATE_LESSON",
-                        updatedLesson: newItem
-                    }))
-        },
-        deleteLesson: (lessonToDelete) => {
-            lessonService.deleteLesson(lessonToDelete._id)
-                .then(status => dispatch({
-                                             type: "DELETE_LESSON",
-                                             lessonToDelete
-                                        }))
-
-        },
-        clearLesson: () =>
-            dispatch({
-                type: "CLEAR_LESSON"
-            })
+        findLessonsForModule: (moduleId) => lessonActions.findLessonsForModule(dispatch, moduleId),
+        createLessonForModule: (moduleId) => lessonActions.createLessonForModule(dispatch, moduleId),
+        updateLesson: (newItem) => lessonActions.updateLesson(dispatch, newItem),
+        deleteLesson: (lessonToDelete) => lessonActions.deleteLesson(dispatch, lessonToDelete),
+        clearLesson: () => lessonActions.clearLesson(dispatch)
     }
 }
 
-export default connect(stpm, dtpm)
-(LessonTabs)
+export default connect(stpm, dtpm)(LessonTabs)
