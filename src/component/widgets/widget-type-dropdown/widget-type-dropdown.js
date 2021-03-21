94f9413a7d93c5d56a902a3dc38a102194fe0a4e
Jiahao Cai <cai.jiah@husky.neu.edu>
2021-03-21 01:56:31 -0400

    implement the componet for widget type down as it is used for all types of widget

---
+++ src/component/widgets/widget-type-dropdown/widget-type-dropdown.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react'
+
+const WidgetTypeDropdown = ({widget,
+                            getTypeChange}) => {
+    return (
+        <select value={widget.type}
+                className="col-10 form-control"
+                onChange={(e)=> getTypeChange(e.target.value)}>
+            <option value={"HEADING"}>Heading</option>
+            <option value={"PARAGRAPH"}>Paragraph</option>
+        </select>
+    )
+}
+
+export default WidgetTypeDropdown
