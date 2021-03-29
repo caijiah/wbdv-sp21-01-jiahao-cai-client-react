7529414ac887c543135d5733dd174a4d5bfea974
Jiahao Cai <cai.jiah@husky.neu.edu>
2021-03-29 17:26:51 -0400

    finish implementing list widget and also set the word-break

--- src/component/widgets/list-widget/list-widget.js
+++ src/component/widgets/list-widget/list-widget.js
@@ -1,21 +1,70 @@
 import React from 'react'
+import "./list-widget.css"
 
 const ListWidget = ({widget,
-                         editing,
-                         updateWidgetText,
-                         updateHeadingSize}) => {
+                    editing,
+                    updateWidgetOrdered,
+                    updateWidgetText
+                    }) => {
 
     return(
         <>
             {
                 editing &&
                 <>
+                    <input checked={widget.ordered}
+                           id = {widget.id}
+                           onChange={(e)=> {
+                               updateWidgetOrdered()
+                           }}
+                           type="checkbox"/>
+                           <label
+                               htmlFor = {widget.id}
+                               className = "ml-1"
+                           > Ordered</label>
+                    <br/>
+                    Item list
+                    <textarea value={widget.text}
+                              onChange={(e)=> {
+                                  updateWidgetText(e.target.value)
+                              }}
+                              rows={10} className="form-control"/>
                 </>
             }
             {
                 !editing &&
                 <>
-
+                    {
+                        widget.ordered &&
+                        <ol>
+                            {
+                                widget.text.split("\n").map((item, index) => {
+                                    return(
+                                        <li
+                                            className="li-break"
+                                            key={index}>
+                                            {item}
+                                        </li>
+                                    )
+                                })
+                            }
+                        </ol>
+                    }
+                    {
+                        !widget.ordered &&
+                        <ul>
+                            {
+                                widget.text.split("\n").map((item, index) => {
+                                    return(
+                                        <li className="li-break"
+                                            key={index}>
+                                            {item}
+                                        </li>
+                                    )
+                                })
+                            }
+                        </ul>
+                    }
                 </>
             }
         </>
