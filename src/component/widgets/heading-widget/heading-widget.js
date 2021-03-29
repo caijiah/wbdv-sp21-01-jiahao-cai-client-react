736d21aafc4de0229d20f0e9da6fa067e6381bc4
Jiahao Cai <cai.jiah@husky.neu.edu>
2021-03-29 17:25:42 -0400

    add work-break of headings

--- src/component/widgets/heading-widget/heading-widget.js
+++ src/component/widgets/heading-widget/heading-widget.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import "./heading-widget.css"
 
 const HeadingWidget = ({widget,
                        editing,
@@ -33,12 +34,12 @@ const HeadingWidget = ({widget,
             {
                 !editing &&
                 <>
-                    {widget.size === 1 && <h1>{widget.text}</h1>}
-                    {widget.size === 2 && <h2>{widget.text}</h2>}
-                    {widget.size === 3 && <h3>{widget.text}</h3>}
-                    {widget.size === 4 && <h4>{widget.text}</h4>}
-                    {widget.size === 5 && <h5>{widget.text}</h5>}
-                    {widget.size === 6 && <h6>{widget.text}</h6>}
+                    {widget.size === 1 && <h1 className='h-break'>{widget.text}</h1>}
+                    {widget.size === 2 && <h2 className='h-break'>{widget.text}</h2>}
+                    {widget.size === 3 && <h3 className='h-break'>{widget.text}</h3>}
+                    {widget.size === 4 && <h4 className='h-break'>{widget.text}</h4>}
+                    {widget.size === 5 && <h5 className='h-break'>{widget.text}</h5>}
+                    {widget.size === 6 && <h6 className='h-break'>{widget.text}</h6>}
                 </>
             }
         </>
