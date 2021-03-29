865fea1884ac30eda2ec7c74fbf631b80eb17a44
Jiahao Cai <cai.jiah@husky.neu.edu>
2021-03-29 17:27:23 -0400

    finish implementing image widget

--- src/component/widgets/image-widget/image-widget.js
+++ src/component/widgets/image-widget/image-widget.js
@@ -1,22 +1,42 @@
 import React from 'react'
 
 const ImageWidget = ({widget,
-                           editing,
-                           updateWidgetText,
-                           updateHeadingSize}) => {
-
+                     editing,
+                     updateWidgetWidth,
+                     updateWidgetHeight,
+                     updateWidgetSrc}) => {
     return(
         <>
             {
                 editing &&
                 <>
+                    Image URL
+                    <input value={widget.src}
+                           type="url"
+                           onChange={(e)=> {
+                               updateWidgetSrc(e.target.value)
+                           }}
+                           className="form-control"/>
+                    Image width
+                    <input value={widget.width}
+                           type="number"
+                           onChange={(e)=> {
+                               updateWidgetWidth(e.target.value)
+                           }}
+                           className="form-control"/>
+                    Image height
+                    <input value={widget.height}
+                           type="number"
+                           onChange={(e)=> {
+                               updateWidgetHeight(e.target.value)
+                           }}
+                           className="form-control"/>
                 </>
             }
             {
                 !editing &&
-                <>
-
-                </>
+                <img width={widget.width} height={widget.height} src={widget.src}
+                     alt={widget.name}/>
             }
         </>
     )
