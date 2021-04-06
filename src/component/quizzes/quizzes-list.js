f9c11988710ed3a75ef80e5232932eb681e9f421
Jiahao Cai <cai.jiah@husky.neu.edu>
2021-04-05 22:51:38 -0400

    implemented quizzes-list using quizzes-service

--- src/component/quizzes/quizzes-list.js
+++ src/component/quizzes/quizzes-list.js
@@ -1,9 +1,39 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import quizApi from "../../services/quizzes-service";
+import {Link, useParams} from "react-router-dom";
 
 const QuizzesList = () => {
+    const {courseId} = useParams();
+    const [quizzes, setQuizzes] = useState([])
+    useEffect(() => {
+        quizApi.findAllQuizzes()
+            .then((quizzes) => {
+                setQuizzes(quizzes)})
+    }, [])
     return (
-        <div>
+        <div className="container">
             <h2>Quizzes</h2>
+            <ul>
+            {
+                quizzes.map((quiz) => {
+                    return(
+                        <li className='list-group'
+                            key={quiz._id}>
+                            <div className='list-group-item'>
+                                <Link
+                                    to={`/courses/${courseId}/quizzes/${quiz._id}`}>
+                                    {quiz.title}
+                                </Link>
+                                <Link to={`/courses/${courseId}/quizzes/${quiz._id}`}
+                                      className="btn btn-primary float-right">
+                                    Start
+                                </Link>
+                            </div>
+                        </li>
+                    )
+                })
+            }
+            </ul>
         </div>
     )
 }
