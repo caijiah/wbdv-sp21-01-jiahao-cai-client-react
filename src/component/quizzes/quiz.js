6cc998e528489d76b2c541c5647b864cc76a7116
Jiahao Cai <cai.jiah@husky.neu.edu>
2021-04-05 23:13:03 -0400

    init all necessary files

---
+++ src/component/quizzes/quiz.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Quiz = () => {
+    return (
+        <div className="container-fluid">
+            <h2>Quiz</h2>
+        </div>
+    )
+}
+
+export default Quiz
