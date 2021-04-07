17b6a44c937e6d42d19f67da97433dd2e74d5ec9
Jiahao Cai <cai.jiah@husky.neu.edu>
2021-04-07 14:35:22 -0400

    create Quiz component, and each quiz should contain questions

--- src/component/quizzes/quiz.js
+++ src/component/quizzes/quiz.js
@@ -1,9 +1,31 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
+import {useParams} from 'react-router-dom'
+import questionApi from "../../services/questions-service";
+import Question from "./questions/question";
 
 const Quiz = () => {
+    const {courseId, quizId} = useParams()
+    const [questions, setQuestions] = useState([])
+
+    useEffect(() => {
+        questionApi.findQuestionsForQuiz(quizId)
+            .then((questions) => setQuestions(questions))
+    }, [])
+
     return (
         <div className="container-fluid">
             <h2>Quiz</h2>
+            <ul>
+            {
+                questions.map(question => {
+                    return (
+                        <li key={question._id} className="list-group">
+                            <Question question={question}/>
+                        </li>
+                    )
+                })
+            }
+            </ul>
         </div>
     )
 }
