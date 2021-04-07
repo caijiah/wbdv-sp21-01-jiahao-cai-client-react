f9070ce177f0f74fde16a6db773f91e77b4447b2
Jiahao Cai <cai.jiah@husky.neu.edu>
2021-04-07 14:53:25 -0400

    follow the wireframes

--- src/component/quizzes/quiz.js
+++ src/component/quizzes/quiz.js
@@ -2,20 +2,25 @@ import React, {useState, useEffect} from "react"
 import {useParams} from 'react-router-dom'
 import questionApi from "../../services/questions-service";
 import Question from "./questions/question";
+import quizApi from "../../services/quizzes-service";
+import './quiz.css'
 
 const Quiz = () => {
     const {courseId, quizId} = useParams()
     const [questions, setQuestions] = useState([])
+    const [quizName, setQuizName] = useState("")
 
     useEffect(() => {
         questionApi.findQuestionsForQuiz(quizId)
             .then((questions) => setQuestions(questions))
-    }, [])
+        quizApi.findQuizById(quizId)
+            .then((quiz) => setQuizName(quiz.title))
+    }, [quizId])
 
     return (
         <div className="container-fluid">
-            <h2>Quiz</h2>
-            <ul>
+            <h2>{quizName}</h2>
+            <ul className='question-list'>
             {
                 questions.map(question => {
                     return (
